5b804810fb18e307183caac17545cd8b26f5d4f0
Viharika <viharika807@gmail.com>
2023-03-19 13:43:10 +0530

    step-3

--- src/Pages/Auth/Signin/index.js
+++ src/Pages/Auth/Signin/index.js
@@ -2,6 +2,84 @@ import { useState, useEffect, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../Context/AuthContext'
 import { LoginIcon } from '@heroicons/react/outline'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`
+const Card = styled.div`
+  width: 400px;
+  padding: 55px;
+  border-radius: 8px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  border: 3px solid yellow;
+  opacity: 1;
+`
+const Title = styled.h2`
+  margin-bottom: 16px;
+  font-size: 40px;
+  font-weight: 600;
+  text-align: center;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+const Input = styled.input`
+  height: 40px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  color: black;
+`
+const Styledsignin = styled.div`
+  text-align: center;
+`
+
+const StyledLink = styled(Link)`
+  color: yellow;
+`
+
+const StyledlogginIcon = styled(LoginIcon)`
+  width: 25px;
+  text-align: center;
+`
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 100%;
+  margin-top: 16px;
+  background-color: yellow;
+  color: black;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: lightyellow;
+  }
+`
 
 const Signin = () => {
 
@@ -31,19 +109,19 @@ const Signin = () => {
   }, [loggedIn])
 
   return (
-    <div>
-      <div>
+    <Container>
+      <Card>
         <div>
-          <h2>Login</h2>
+          <Title>Login</Title>
         </div>
-        <form
+        <Form
           autoComplete="off"
           onSubmit={handleSignIn}
         >
           <div>
-            <div>
-              <label>Email</label>
-              <input
+            <FormGroup>
+              {/* <label>Email</label> */}
+              <Input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
@@ -51,10 +129,10 @@ const Signin = () => {
                 placeholder="Email Address"
                 required
               />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
+            </FormGroup>
+            <FormGroup>
+              {/* <label>Password</label> */}
+              <Input
                 type="Password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
@@ -62,28 +140,31 @@ const Signin = () => {
                 placeholder="Password"
                 ref={passwordRef}
               />
-            </div>
-            <div>
+            </FormGroup>
+
+            <Styledsignin>
               <div>
                 <span>
                   Don't have an account? Sign up{" "}
-                  <Link to="/signup">
+                  <StyledLink to="/signup">
                     {" "}
                     here.
-                  </Link>
+                  </StyledLink>
                 </span>
               </div>
-            </div>
+            </Styledsignin>
+
             <div>
-              <button type="submit">
-                <LoginIcon aria1-hidden="true" />
+              <Button type="submit">
+                <StyledlogginIcon aria1-hidden="true" />
                 Login
-              </button>
+              </Button>
             </div>
+
           </div>
-        </form>
-      </div>
-    </div>
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
--- src/Pages/Auth/Signup/index.js
+++ src/Pages/Auth/Signup/index.js
@@ -3,6 +3,94 @@ import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../Context/AuthContext'
 import validations from './validations'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`
+
+const Card = styled.div`
+  width: 400px;
+  padding: 30px 55px;
+  border-radius: 8px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  opacity: 1;
+`
+
+const Title = styled.h2`
+  margin-bottom: 16px;
+  font-size: 40px;
+  font-weight: 600;
+  text-align: center;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+const Input = styled.input`
+  height: 40px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  width: 100%;
+  color: black;
+`
+const Styledsignup = styled.div`
+  text-align: center;
+`
+
+const StyledLink = styled(Link)`
+  color: yellow;
+`
+
+const StyledsignupIcon = styled(IdentificationIcon)`
+  width: 25px;
+  text-align: center;
+`
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 100%;
+  margin-top: 16px;
+  background-color: yellow;
+  color: black;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  &:hover {
+    background-color: lightyellow;
+  }
+`
+
+const ErrorMessage = styled.span`
+  color: red;
+  font-size: 14px;
+  margin-top: 8px;
+`
+
+
 
 const Signup = () => {
   const {
@@ -34,20 +122,20 @@ const Signup = () => {
   }
 
   return (
-    <div >
-      <div >
+    <Container >
+      <Card >
         <div>
-          <h2 >Sign Up</h2>
+          <Title >Sign Up</Title>
         </div>
-        <form
+        <Form
           autoComplete="off"
           onSubmit={handleSignUpSubmit}
         >
           <div >
-            <div>
-            {errors.firstName && <span>{errors.firstName}</span>}
-              <label >First Name</label>
-              <input
+            <FormGroup>
+            {errors.firstName && <ErrorMessage>{errors.firstName}</ErrorMessage>}
+              {/* <label >First Name</label> */}
+              <Input
                 type="text"
                 onChange={handleSignUpFormChange}
                 value={currentUser.firstName}
@@ -55,12 +143,12 @@ const Signup = () => {
                 placeholder="First Name"
               />
               
-            </div>
+            </FormGroup>
 
-            <div>
-            {errors.lastName && <span>{errors.lastName}</span>}
-              <label>Last Name</label>
-              <input
+            <FormGroup>
+            {errors.lastName && <ErrorMessage>{errors.lastName}</ErrorMessage>}
+              {/* <label>Last Name</label> */}
+              <Input
                 type="text"
                 onChange={handleSignUpFormChange}
                 value={currentUser.lastName}
@@ -68,11 +156,12 @@ const Signup = () => {
                 placeholder="Last Name"
               />
               
-            </div>
-            <div>
-            {errors.email && <span >{errors.email}</span>}
-              <label>Email</label>
-              <input
+            </FormGroup>
+
+            <FormGroup>
+            {errors.email && <ErrorMessage >{errors.email}</ErrorMessage>}
+              {/* <label>Email</label> */}
+              <Input
                 type="email"
                 onChange={handleSignUpFormChange}
                 value={currentUser.email}
@@ -80,11 +169,12 @@ const Signup = () => {
                 placeholder="Email Address"
               />
               
-            </div>
-            <div>
-            {errors.password && <span >{errors.password}</span>}
-              <label >Password</label>
-              <input
+            </FormGroup>
+
+            <FormGroup>
+            {errors.password && <ErrorMessage >{errors.password}</ErrorMessage>}
+              {/* <label >Password</label> */}
+              <Input
                 type="Password"
                 onChange={handleSignUpFormChange}
                 value={currentUser.password}
@@ -92,11 +182,12 @@ const Signup = () => {
                 placeholder="Password"
               />
               
-            </div>
-            <div>
-            {errors.passwordConfirm && <span>{errors.passwordConfirm}</span>}
-              <label >Password Confirm</label>
-              <input
+            </FormGroup>
+
+            <FormGroup>
+            {errors.passwordConfirm && <ErrorMessage>{errors.passwordConfirm}</ErrorMessage>}
+              {/* <label >Password Confirm</label> */}
+              <Input
                 type="Password"
                 onChange={handleSignUpFormChange}
                 value={currentUser.passwordConfirm}
@@ -104,30 +195,33 @@ const Signup = () => {
                 placeholder="Password Confirm"
               />
               
-            </div>
-            <div >
+            </FormGroup>
+
+            <Styledsignup >
               <div >
                 <span>
                   Already have an account? Login{" "}
-                  <Link to="/signin" >
+                  <StyledLink to="/signin" >
                     {" "}
                     here.
-                  </Link>
+                  </StyledLink>
                 </span>
               </div>
-            </div>
+            </Styledsignup>
+
             <div >
-              <button type="submit" >
-                <IdentificationIcon
+              <Button type="submit" >
+                <StyledsignupIcon
                   aria1-hidden="true"
                 />
                 Sign Up
-              </button>
+              </Button>
             </div>
+
           </div>
-        </form>
-      </div>
-    </div>
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
